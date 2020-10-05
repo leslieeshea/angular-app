c0bfdc4362f62004e7068039cd2298b3c64595f3
Leslie Shea <leslieyshea@gmail.com>
2020-10-05 15:25:31 -0700

    added routes for home component and breweries component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { BreweriesComponent } from './breweries/breweries.component';
+
+const routes: Routes = [
+  // the component that has a path empty will load first
+  { path: '', component: HomeComponent },
+  { path: 'breweries', component: BreweriesComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { ListComponent } from './list/list.component';
 import { BreweriesComponent } from './breweries/breweries.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    ListComponent,
     BreweriesComponent
   ],
   imports: [
