7a149c6192737d5fe1917e29b52c340cb77ba3ce
Leslie Shea <leslieyshea@gmail.com>
2020-10-05 18:00:52 -0700

    should retrieve list of breweries test passing

--- src/app/http.service.spec.ts
+++ src/app/http.service.spec.ts
@@ -1,16 +1,31 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { HttpService } from './http.service';
 
 describe('HttpService', () => {
   let service: HttpService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule], // this will simulate a real api call
+      providers: [
+        HttpService,
+
+      ]
+    });
     service = TestBed.inject(HttpService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should retrieve list of breweries', () => {
+    service.getBreweries().subscribe();
+    const req = httpMock.expectOne(service.url);
+    expect(req.request.method).toBe('GET');
   });
 });
