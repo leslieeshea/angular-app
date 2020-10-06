674c695656636abd6f685d208d2a249a53848040
Leslie Shea <leslieyshea@gmail.com>
2020-10-05 17:24:12 -0700

    able to render list of breweries on page

--- src/app/breweries/breweries.component.ts
+++ src/app/breweries/breweries.component.ts
@@ -8,14 +8,14 @@ import { HttpService } from '../http.service';
 })
 export class BreweriesComponent implements OnInit {
   // you need this to store the data that you're getting back from the get request
-  brews: Object;
+  breweries: Object;
 
   constructor(private http: HttpService) { }
 
   ngOnInit() { // anything inside here runs when the component is loaded
     this.http.getBreweries().subscribe(data => {
-      this.brews = data; // this will store the data pulled from the api get request and set it to the brews object
-      console.log(this.brews);
+      this.breweries = data; // this will store the data pulled from the api get request and set it to the brews object
+      console.log(this.breweries);
     });
   }
 
